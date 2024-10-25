22dff636b0ecbeb2b45897f111ec2fa657c4b70d
jtam90 Tamb <jas.tamburro@gmail.com>
2024-10-25 11:51:25 +0000

    Fix button container

--- faefolkfantasium/models.py
+++ faefolkfantasium/models.py
@@ -4,7 +4,6 @@ class Being(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    is_example = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return f"<Being {self.name}>"
