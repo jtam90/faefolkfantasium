63416c2364f92e9fa2d4856fa14c21ee3fb3b24e
jtam90 Tamb <jas.tamburro@gmail.com>
2024-11-08 16:40:46 +0000

    Fix db path error

--- faefolkfantasium/__init__.py
+++ faefolkfantasium/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/beings.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////workspace/faefolkfantasium/instance/beings.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 print(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
