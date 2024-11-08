b9ffb52e53c641b0070ce58f1f49e99e58c3b65b
jtam90 Tamb <jas.tamburro@gmail.com>
2024-11-08 14:32:55 +0000

    Fix duplicate db error

--- faefolkfantasium/__init__.py
+++ faefolkfantasium/__init__.py
@@ -3,8 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///beings.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/beings.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+print(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
+
 
 db = SQLAlchemy(app)
 
