77401a2682694910ae0fd320899addf1db3c9e2e
jtam90 Tamb <jas.tamburro@gmail.com>
2024-12-11 06:14:30 +0000

    Fix port binding for Heroku

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
-import webbrowser
-from faefolkfantasium.app import create_app
-
-# Create the app instance
-app = create_app()
+import os
+from faefolkfantasium import app
 
 if __name__ == "__main__":
-    # Open the browser automatically to the correct Gitpod port
-    webbrowser.open("http://localhost:8081")
-    app.run(host="0.0.0.0", port=8080, debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    app.run(host="0.0.0.0", port=port, debug=False)  # Use host="0.0.0.0" for Heroku
+
 
 
 
