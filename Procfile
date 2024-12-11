2ec9da94786ecaa6bb74c1f3994b72b1e0d43565
jtam90 Tamb <jas.tamburro@gmail.com>
2024-12-11 06:19:44 +0000

    Fix Heroku port binding and use Gunicorn

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-web: gunicorn faefolkfantasium:app
+web: gunicorn faefolkfantasium.app:app
 web: flask run --host=0.0.0.0 --port=$PORT
 web: python run.py
 
--- run.py
+++ run.py
@@ -2,8 +2,10 @@ import os
 from faefolkfantasium import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
-    app.run(host="0.0.0.0", port=port, debug=False)  # Use host="0.0.0.0" for Heroku
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
 
 
